8bb18d416738b3c283e63efa0c483637ac03e1aa
Jorge Guerra <jorguerra@gmail.com>
2019-01-30 20:30:29 -0300

    Create and list Todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,5 +3,18 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
-  
+
+  def new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+
 end
